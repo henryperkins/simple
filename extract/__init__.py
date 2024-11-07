eb761c1ad75f30fbbf4cbf22b5d0d7f2d64baadf
Henry Perkins <htperkins@gmail.com>
2024-11-07 14:51:34 -0600

    Update __init__.py

--- extract/__init__.py
+++ extract/__init__.py
@@ -10,13 +10,14 @@ Modules:
     - functions: FunctionExtractor for extracting function details.
     - utils: Utility functions for AST manipulation and annotation processing.
 """
-
+from .base import BaseExtractor
 from .code import extract_classes_and_functions_from_ast
 from .functions import FunctionExtractor
 from .classes import ClassExtractor
 from .utils import add_parent_info, get_annotation
 
 __all__ = [
+    "BaseExtractor"
     "extract_classes_and_functions_from_ast",
     "FunctionExtractor",
     "ClassExtractor",
