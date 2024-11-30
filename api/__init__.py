01d7a7e81be7ad05b9e63ac84d32232b9052a1e4
Henry Perkins <htperkins@gmail.com>
2024-11-29 21:09:50 -0600

    up

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,11 @@
 # api/__init__.py
 from .api_client import APIClient
 from .response_parser import ResponseParser
-from .token_management import TokenManager
+from .token_management import (
+    TokenManager, TokenUsage, estimate_tokens, chunk_text
+)
 
-__all__ = ['APIClient', 'ResponseParser', 'TokenManager']
+__all__ = [
+    'APIClient', 'ResponseParser', 'TokenManager', 'TokenUsage',
+    'estimate_tokens', 'chunk_text'
+]
--- core/__init__.py
+++ core/__init__.py
@@ -1,15 +1,34 @@
 # core/__init__.py
 from .cache import Cache
-from .code_extraction import CodeExtractor, ExtractionContext
+from .code_extraction import (
+    CodeExtractor, ExtractionContext, ExtractedArgument, ExtractedElement,
+    ExtractedFunction, ExtractionResult, ExtractedClass
+)
 from .config import AzureOpenAIConfig
-from .docstring_processor import DocstringProcessor
-from .logger import LoggerSetup
-from .metrics import Metrics
+from .docs import DocumentationError, DocumentationContext, DocStringManager
+from .docstring_processor import (
+    DocstringData, DocstringMetrics, DocumentationSection, DocstringProcessor
+)
+from .logger import LoggerSetup, log_debug, log_info, log_warning, log_error
+from .markdown_generator import MarkdownConfig, MarkdownGenerator
+from .metrics import MetricsError, Metrics
 from .monitoring import SystemMonitor, MetricsCollector
-from .utils import generate_hash, get_annotation
+from .utils import (
+    generate_hash, get_annotation, handle_exceptions, load_json_file,
+    ensure_directory, validate_file_path, create_error_result, add_parent_info,
+    get_file_stats, filter_files, get_all_files
+)
 
 __all__ = [
-    'Cache', 'CodeExtractor', 'ExtractionContext', 'AzureOpenAIConfig',
-    'DocstringProcessor', 'LoggerSetup', 'Metrics', 'SystemMonitor',
-    'MetricsCollector', 'generate_hash', 'get_annotation'
+    'Cache', 'CodeExtractor', 'ExtractionContext', 'ExtractedArgument',
+    'ExtractedElement', 'ExtractedFunction', 'ExtractionResult',
+    'ExtractedClass', 'AzureOpenAIConfig', 'DocumentationError',
+    'DocumentationContext', 'DocStringManager', 'DocstringData',
+    'DocstringMetrics', 'DocumentationSection', 'DocstringProcessor',
+    'LoggerSetup', 'log_debug', 'log_info', 'log_warning', 'log_error',
+    'MarkdownConfig', 'MarkdownGenerator', 'MetricsError', 'Metrics',
+    'SystemMonitor', 'MetricsCollector', 'generate_hash', 'get_annotation',
+    'handle_exceptions', 'load_json_file', 'ensure_directory',
+    'validate_file_path', 'create_error_result', 'add_parent_info',
+    'get_file_stats', 'filter_files', 'get_all_files'
 ]
